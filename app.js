44e26939d7b56a9f32de62ed379c1d495c1481c7
Dardan Bajraktari <bajraktaridardan5@gmail.com>
2019-12-25 19:31:11 +0000

    Added ability to move points around

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let animationSpeed = 1;
+
 class Point {
     constructor(xPosition, yPosition, speed) {
         this.xPosition = xPosition;
@@ -17,12 +19,38 @@ class Algorithm {
 }
 
 class UI {
-    constructor() {
-        this.animationSpeed = 1;
-    }
-
     addPoint() {
 
+    }
+
+    dragPoint(element) {
+        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        element.onmousedown = dragMouseDown;
+
+        function dragMouseDown(event) {
+            event = event || window.event;
+            event.preventDefault();
+            pos3 = event.clientX;
+            pos4 = event.clientY;
+            document.onmouseup = closeDragPoint;
+            document.onmousemove = elementDrag;
+        }
+        
+        function elementDrag(event) {
+            event = event || window.event;
+            event.preventDefault();
+            pos1 = pos3 - event.clientX;
+            pos2 = pos4 - event.clientY;
+            pos3 = event.clientX;
+            pos4 = event.clientY;
+            element.style.top = (element.offsetTop - pos2) + "px";
+            element.style.left = (element.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragPoint() {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
     }
 
     removePoint() {
@@ -40,8 +68,11 @@ class UI {
     runFindConstantMotionMeet() {
 
     }
+}
 
-    setAnimationSpeed(animationSpeed) {
-        this.animationSpeed = animationSpeed;
-    }
+dragPoint(document.getElementById('point1'));
+
+function dragPoint(element) {
+    const uI = new UI;
+    uI.dragPoint(element);
 }

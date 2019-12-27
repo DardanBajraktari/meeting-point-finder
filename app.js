81eb5c2a0e9680226b54d04c1705b5011b9ee145
Dardan Bajraktari <bajraktaridardan5@gmail.com>
2019-12-27 19:56:45 +0000

    Can now display points in local storage and open points settings on click

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 class Point {
-    constructor(name, xPosition, yPosition, speed) {
+    constructor(name, id, xPosition, yPosition, speed) {
         this.name = name;
+        this.id = id;
         this.xPosition = xPosition;
         this.yPosition = yPosition;
         this.speed = speed;
@@ -9,16 +10,38 @@ class Point {
 
 class Points {
     static getPoints() {
-        let points = [];
+        let points;
+
+        if (localStorage.getItem('points') === null) {
+            points = [];
+        } else {
+            points = JSON.parse(localStorage.getItem('points'));
+        }
 
         return points;
     }
 
     static addPoint() {
         const points = Points.getPoints();
-        const point = new Point('Unnamed point', 200, 200, 1);
+        let id = 'point' + (points.length + 1);
+        const point = new Point('Unnamed point', id, 200, 200, 1);
 
-        points.push(point);        
+        points.push(point);
+        localStorage.setItem('points', JSON.stringify(points));
+        console.log(points);      
+    }
+
+    static removePoint() {
+
+    }
+
+    static clearPoints() {
+
+    }
+
+    static displayPoints() {
+        const uI = new UI();
+        uI.displayPoints();
     }
 }
 
@@ -37,44 +60,80 @@ class UI {
         this.animationSpeed = 1;
     }
 
-    addPoint() {
+    drawPoint(point) {
         const newPoint = document.createElement('div');
         newPoint.classList.add('point');
 
-        const points = Points.getPoints();
-        newPoint.id = 'point' + points.length;
-        this.dragPoint(newPoint);
+        if (point) {
+            newPoint.id = point.id;
+        } else {
+            const points = Points.getPoints();
+            newPoint.id = points[points.length - 1].id;
+        }
+
         document.body.append(newPoint);
+
+        this.dragPoint(newPoint);
+        this.getInfoOnClick(newPoint);
     }
 
-    dragPoint(element) {
+    dragPoint(point) {
         let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-        element.onmousedown = dragMouseDown;
+        point.onmousedown = dragMouseDown;
 
         function dragMouseDown(event) {
             event = event || window.event;
             event.preventDefault();
+
             pos3 = event.clientX;
             pos4 = event.clientY;
+
+            document.onmousemove = pointDrag;
             document.onmouseup = closeDragPoint;
-            document.onmousemove = elementDrag;
         }
         
-        function elementDrag(event) {
+        function pointDrag(event) {
             event = event || window.event;
             event.preventDefault();
+
             pos1 = pos3 - event.clientX;
             pos2 = pos4 - event.clientY;
             pos3 = event.clientX;
             pos4 = event.clientY;
-            element.style.top = (element.offsetTop - pos2) + "px";
-            element.style.left = (element.offsetLeft - pos1) + "px";
+
+            point.style.top = (point.offsetTop - pos2) + "px";
+            point.style.left = (point.offsetLeft - pos1) + "px";
         }
 
         function closeDragPoint() {
             document.onmouseup = null;
             document.onmousemove = null;
         }
+    }
+
+    getInfoOnClick(point) {
+        let self = this;
+        console.log(point);
+        point.addEventListener('click', function () {
+            self.openPointSettings(point);
+        });
+    }
+
+    displayPoints() {
+        let self = this;
+        const points = Points.getPoints();
+        
+        points.forEach(function (point) {
+            self.drawPoint(point);
+        });
+    }
+
+    openPointSettings(point) {
+        document.getElementById('point-settings').classList.add('side-nav-view');
+    }
+
+    closePointSettings() {
+        document.getElementById('point-settings').classList.remove('side-nav-view');
     }
 
     removePoint() {
@@ -85,17 +144,19 @@ class UI {
 
     }
 
-    runFindClosestMeet () {
+    findClosestMeet () {
 
     }
 
-    runFindConstantMotionMeet() {
+    findConstantMotionMeet() {
 
     }
 }
 
+document.addEventListener('DOMContentLoaded', Points.displayPoints);
+
 document.getElementById('add-new-point').addEventListener('click', function () {
     Points.addPoint();
     const uI = new UI();
-    uI.addPoint();
+    uI.drawPoint();
 });
